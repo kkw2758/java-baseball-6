8246390cb713405dac0e2692aefb1629855ea164
kkw2758 <dtc3229@gmail.com>
2023-11-21 17:40:35 +0900

    Feat : 기본적으로 사용하는 입력/출력 메서드 추가

---
+++ src/main/java/baseball/view/console/ConsoleReader.java
@@ -0,0 +1,22 @@
+package baseball.view.console;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public final class ConsoleReader {
+    public static String enterMessage() {
+        return Validator.validate(Console.readLine());
+    }
+
+    private static class Validator {
+        public static String validate(String message) {
+            validateBlankInput(message);
+            return message;
+        }
+
+        private static void validateBlankInput(String message) {
+            if (message.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
