66d15bf0f584a1a32f26ae4aff86600ef6179a3c
kkw2758 <dtc3229@gmail.com>
2023-11-21 17:44:39 +0900

    Feat(Numbers) : Numbers 도메인에서의 숫자 검증 기능 추가
    - 입력 받은 숫자가 세자리 숫자가 아니면 예외 처리한다.
    - 입력 받은 숫자에 중복된 숫자가 있으면 예외 처리한다.

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -9,10 +9,17 @@ public class Numbers {
     private final List<Number> numbers;
 
     private Numbers(List<Number> numbers) {
+        validateNumbers(numbers);
         this.numbers = numbers;
     }
 
     public static Numbers generateRandomNumbers() {
+        return new Numbers(createRandomNumbers().stream()
+                .map(Number::from)
+                .toList());
+    }
+
+    private static List<Integer> createRandomNumbers() {
         List<Integer> randomNumbers = new ArrayList<>();
         while (randomNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -20,8 +27,39 @@ public class Numbers {
                 randomNumbers.add(randomNumber);
             }
         }
-        return new Numbers(randomNumbers.stream()
+        return randomNumbers;
+    }
+
+    public static Numbers generateNumbers(List<Integer> numbers) {
+        return new Numbers(numbers.stream()
                 .map(Number::from)
                 .toList());
+    }
+
+    private void validateNumbers(List<Number> numbers) {
+        validateLength(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateDuplicate(List<Number> numbers) {
+        if (checkDuplicate(numbers)) {
+            throw new IllegalArgumentException("서로 다른 3개의 숫자를 입력해야합니다.");
+        }
+    }
+
+    private boolean checkDuplicate(List<Number> numbers) {
+        return numbers.stream()
+                .distinct()
+                .toList().size() != numbers.size();
+    }
+
+    private void validateLength(List<Number> numbers) {
+        if (checkNumbersLength(numbers)) {
+            throw new IllegalArgumentException("서로 다른 3개의 숫자를 입력해야합니다.");
+        }
+    }
+
+    private boolean checkNumbersLength(List<Number> numbers) {
+        return numbers.size() != 3;
     }
 }
