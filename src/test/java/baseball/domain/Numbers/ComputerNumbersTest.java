de191246c0c1ea99042c6e26c9217e538d5c6337
kkw2758 <dtc3229@gmail.com>
2023-11-22 20:09:02 +0900

    Test : Numbers 클래스 삭제 후 새로 생성한 클래스 ComputerNumbers에 대한 테스트 코드 작성

--- src/test/java/baseball/domain/Numbers/ComputerNumbersTest.java
+++ src/test/java/baseball/domain/Numbers/ComputerNumbersTest.java
@@ -1,19 +1,18 @@
-package baseball.domain;
+package baseball.domain.Numbers;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class NumbersTest {
-
+class ComputerNumbersTest {
     @Test
     @DisplayName("입력 받은 숫자가 세자리 숫자가 아니면 예외 처리한다.")
     void numbersLengthExceptionTest() {
         //given & when & then
-        assertThatThrownBy(() -> Numbers.generateNumbers(List.of(1, 2, 3, 4)))
+        assertThatThrownBy(() -> ComputerNumbers.of(List.of(1, 2, 3, 4)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -21,7 +20,7 @@ class NumbersTest {
     @DisplayName("입력 받은 숫자에 중복된 숫자가 있으면 예외 처리한다.")
     void numbersDuplicateExceptionTest() {
         //given & when & then
-        assertThatThrownBy(() -> Numbers.generateNumbers(List.of(1, 2, 2)))
+        assertThatThrownBy(() -> ComputerNumbers.of(List.of(1, 2, 2)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -29,11 +28,11 @@ class NumbersTest {
     @DisplayName("사용자의 숫자와 컴퓨터의 숫자를 비교하여 같은 위치에 같은 값을 가진 숫자의 개수를 구한다.")
     void calculateMatchNumberCountTest() {
         //given
-        Numbers userNumbers = Numbers.generateNumbers(List.of(1, 2, 3));
-        Numbers computerNumbers = Numbers.generateNumbers(List.of(3, 2, 1));
+        UserNumbers userNumbers = UserNumbers.of(List.of(1, 2, 3));
+        ComputerNumbers computerNumbers = ComputerNumbers.of(List.of(3, 2, 1));
 
         //when
-        int result = userNumbers.calculateMatchNumberCount(computerNumbers);
+        int result = computerNumbers.calculateMatchNumberCount(userNumbers);
         int expectedResult = 1;
 
         //then
@@ -44,11 +43,11 @@ class NumbersTest {
     @DisplayName("사용자의 숫자와 컴퓨터의 숫자를 비교하여 같은 값을 가진 숫자의 개수를 구한다.")
     void calculateSameNumberCountTest() {
         //given
-        Numbers userNumbers = Numbers.generateNumbers(List.of(1, 2, 3));
-        Numbers computerNumbers = Numbers.generateNumbers(List.of(3, 2, 1));
+        UserNumbers userNumbers = UserNumbers.of(List.of(1, 2, 3));
+        ComputerNumbers computerNumbers = ComputerNumbers.of(List.of(3, 2, 1));
 
         //when
-        int result = userNumbers.calculateSameNumberCount(computerNumbers);
+        int result = computerNumbers.calculateSameNumberCount(userNumbers);
         int expectedResult = 3;
 
         //then
